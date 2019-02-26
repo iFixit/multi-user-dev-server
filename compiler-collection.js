55b6dc77d8906aabfd8e5e6cde96e714a0560820
Daniel Beardsley <daniel@ifixit.com>
2019-02-26 01:49:11 -0700

    CompilerCollection: use new Map()
    It better represents what we're doing here and is
    easy to iterate over.

--- compiler-collection.js
+++ compiler-collection.js
@@ -1,14 +1,14 @@
 modules.exports = () => {
-  const compilers = {}
+  const compilers = new Map();
 
   return {
-    get: (username) => compilers[username],
-    set: (username, compiler) => compilers[username] = compiler,
+    get: (username) => compilers.get(username),
+    set: (username, compiler) => compilers.set(username, compiler),
     remove: (username) => {
-      const compiler = compilers[username];
+      const compiler = compilers.get(username);
       if (!compiler) return;
       compiler.watching.close();
-      delete compilers[username];
+      compilers.delete(username);
     }
   }
 }
