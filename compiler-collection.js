525013f83d1926f946a289253d25da9c15c8cbe4
Daniel Beardsley <daniel@ifixit.com>
2019-02-26 02:18:08 -0700

    CC: keep track of lastAccessed time
    for future expiration logic

--- compiler-collection.js
+++ compiler-collection.js
@@ -1,12 +1,20 @@
 module.exports = () => {
   const compilers = new Map();
 
+  function updateLastAccessed(username) {
+    const compiler = compilers.get(username)
+    if (!compiler) return;
+    compiler.lastAccessed = Date.now();
+  }
+
   return {
     get: (username) => {
+      updateLastAccessed(username)
       return compilers.get(username)
     },
     set: (username, compiler) => {
       compilers.set(username, compiler)
+      updateLastAccessed(username)
     },
     remove: (username) => {
       const compiler = compilers.get(username);
