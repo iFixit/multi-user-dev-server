1c3c7727dccc56cb3c8b477a59db70d3081d5aa8
Daniel Beardsley <daniel@ifixit.com>
2019-03-14 13:10:46 -0700

    Compiler-collection: use individual timeouts for expiration
    Much simpler, and more accurate!

--- compiler-collection.js
+++ compiler-collection.js
@@ -2,40 +2,24 @@ module.exports = (expireAfterSeconds) => {
   const expireMs = expireAfterSeconds * 1000;
   const compilers = new Map();
 
-  if (expireAfterSeconds) {
-    // Check for expired things often enough to expire things within
-    // 20% of their true expiration time.
-    setInterval(removeExpiredCompilers, expireAfterSeconds / 5);
-  }
-
-  function removeExpiredCompilers() {
-    compilers.forEach((compiler, username) => {
-      if (isExpired(compiler)) {
-        console.log(`${username}: bundle was unused for ${expireAfterSeconds} seconds, expiring.`);
-        collection.remove(username);
-      }
-    })
-  }
-
-  function isExpired(compiler) {
-    const oldestAllowedAccessTime = Date.now() - expireMs;
-    return compiler.lastAccessed < oldestAllowedAccessTime;
-  }
-
-  function updateLastAccessed(username) {
+  function resetExpireTimeout(username) {
     const compiler = compilers.get(username)
-    if (!compiler) return;
-    compiler.lastAccessed = Date.now();
+    if (!compiler || !expireMs) {
+       return;
+    }
+    clearTimeout(compiler.expiredTimeout);
+    compiler.expiredTimeout = setTimeout(() => collection.remove(username), expireMs);
+    console.log(`${username}: bundle was unused for ${expireAfterSeconds} seconds, expiring.`);
   }
 
   const collection = {
     get: (username) => {
-      updateLastAccessed(username)
+      resetExpireTimeout(username)
       return compilers.get(username)
     },
     set: (username, compiler) => {
       compilers.set(username, compiler)
-      updateLastAccessed(username)
+      resetExpireTimeout(username)
     },
     remove: (username) => {
       const compiler = compilers.get(username);
